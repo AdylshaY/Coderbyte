b11a8fb876a47834792e32a7e4f2d6742e31e597
Adylsha Yumayev <68568793+AdylshaY@users.noreply.github.com>
2022-02-03 09:35:12 +0300

    Update ArrayCouples.py

--- ArrayCouples.py
+++ ArrayCouples.py
@@ -1,16 +1,17 @@
-# This code got 6 points !!!
-
 def ArrayCouples(arr):
   pair_list = FindPairs(arr)
+  l = []
   for pair in pair_list:
-    pair.reverse()
-    pair_list.remove(pair)
-    if pair in pair_list:
+    reversed_pair = list(reversed(pair))
+    if reversed_pair in pair_list:
       continue
     else:
-      return ",".join(list(map(str, pair)))
-  return 'yes'
-
+      l.append(pair)
+  if len(l) == 0:
+    return 'yes'
+  else:
+    flatten_list = [item for subl in l for item in subl]
+    return ",".join(list(map(str, flatten_list)))
 
 def FindPairs(arr):
   pairs = []
